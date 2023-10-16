b3e17f437f292e48ce0179ac4a5463a5587ea4c7
602822 <89077126+602822@users.noreply.github.com>
2023-10-16 14:42:19 +0200

    all tests passed

--- src/main/java/no/hvl/dat152/rest/ws/controller/AuthorController.java
+++ src/main/java/no/hvl/dat152/rest/ws/controller/AuthorController.java
@@ -6,16 +6,15 @@ package no.hvl.dat152.rest.ws.controller;
 
 import no.hvl.dat152.rest.ws.exceptions.AuthorNotFoundException;
 import no.hvl.dat152.rest.ws.model.Author;
+import no.hvl.dat152.rest.ws.model.Book;
 import no.hvl.dat152.rest.ws.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 
 /**
@@ -49,5 +48,31 @@ public class AuthorController {
         return new ResponseEntity<>(authors, HttpStatus.OK);
     }
 
+    @PostMapping("/authors")
+    public ResponseEntity<Object> createAuthor(@RequestBody Author author) {
+        Author newAuthor = authorService.createAuthor(author);
+        return new ResponseEntity<>(newAuthor, HttpStatus.CREATED);
+    }
+
+    @PutMapping("authors/{authorId}")
+    public ResponseEntity<Object> updateAuthor(@PathVariable("authorId") int authorId, @RequestBody Author author) {
+        try {
+            authorService.updateAuthor(author,authorId);
+            return new ResponseEntity<>(author, HttpStatus.OK);
+        } catch (AuthorNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("authors/{authorId}/books")
+    public ResponseEntity<Object> getAuthorBooks(@PathVariable("authorId") int authorId) throws AuthorNotFoundException {
+        try {
+     Set<Book> books =  authorService.getAuthorBooksOfAuthor(authorId);
+            return new ResponseEntity<>(books, HttpStatus.OK);
+        } catch (AuthorNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NO_CONTENT);
+        }
+
+    }
 
 }
--- src/main/java/no/hvl/dat152/rest/ws/service/AuthorService.java
+++ src/main/java/no/hvl/dat152/rest/ws/service/AuthorService.java
@@ -1,9 +1,11 @@
 /**
- * 
+ *
  */
 package no.hvl.dat152.rest.ws.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,27 +22,58 @@ import no.hvl.dat152.rest.ws.repository.AuthorRepository;
 @Service
 public class AuthorService {
 
-	@Autowired
-	private AuthorRepository authorRepository;
-	
-	
-	public Author saveAuthor(Author author) {
+    @Autowired
+    private AuthorRepository authorRepository;
 
-		author = authorRepository.save(author);
-		// TODO
-		return author;
-	}
-	
-	public Author findById(long id) throws AuthorNotFoundException {
-		
-		Author author = authorRepository.findById(id)
-				.orElseThrow(()-> new AuthorNotFoundException("Author with the id: "+id+ "not found!"));
-		
-		return author;
-	}
 
-	public List<Author> findAll() {
-		return(List<Author>) authorRepository.findAll();
-	}
+    public Author saveAuthor(Author author) {
+
+        author = authorRepository.save(author);
+        // TODO
+        return author;
+    }
+
+    public Author findById(long id) throws AuthorNotFoundException {
+
+        Author author = authorRepository.findById(id)
+                .orElseThrow(() -> new AuthorNotFoundException("Author with the id: " + id + "not found!"));
+
+        return author;
+    }
+
+    public List<Author> findAll() {
+        return (List<Author>) authorRepository.findAll();
+    }
+
+
+    public Author createAuthor(Author author) {
+        authorRepository.save(author);
+        return author;
+    }
+
+    public Author updateAuthor(Author author, int authorId) throws AuthorNotFoundException {
+
+        Optional<Author> authorFromRepo = authorRepository.findById((long) authorId);
+
+        if (authorFromRepo.isEmpty()) {
+            throw new AuthorNotFoundException("Author with id: " + authorId + " could not be found");
+        }
+        int id = authorFromRepo.get().getAuthorId();
+        author.setAuthorId(id);
+        authorRepository.save(author);
+
+        return author;
+    }
+
+    public Set<Book> getAuthorBooksOfAuthor( int authorId) throws AuthorNotFoundException {
+        Optional<Author> authorFromRepo = authorRepository.findById((long) authorId);
+
+        if (authorFromRepo.isEmpty()) {
+            throw new AuthorNotFoundException("Author with id: " + authorId + " could not be found");
+        }
+
+        Set<Book> books = authorFromRepo.get().getBooks();
+      return books;
+    }
 
 }
